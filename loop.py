49175d1859e3db26670b75ab0d56ac37d5487a07
TauTheLepton <matipalczuk@gmail.com>
2022-06-18 21:00:18 +0200

    modified loop.py to create different cases for comparison graphs

--- loop.py
+++ loop.py
@@ -4,19 +4,19 @@ import random
 
 def main():
     # e+5 iters liczy sie ok 14 minut (troche ponad) (populacja 100; n 4)
-    proc = 10 # liczba rdzeni do uzycia
+    proc = 11 # liczba rdzeni do uzycia
     iters = 2 # ile razy ma sie policzyc (na kazdym procesorze, czyli bedzie iters * proc wynikow)
-    bias = 10 # od jakiego numeru ma zaczac nazywac pliki
+    bias = 5000 # od jakiego numeru ma zaczac nazywac pliki
     params = {
         "m": 20,
-        "n": 6,
+        "n": 5,
         "mini": -10,
         "maks": 10,
         "iters": 1 * 10**5,
-        "pop_size": 200,
+        "pop_size": 100,
         "pm": 0.01,
         "pc": 0.7,
-        "seed_ga": 1,
+        "seed_ga": 32493,
         "seed_game": 2137,
         "set_seed_ga": True,
         "set_seed_game": True
@@ -25,12 +25,13 @@ def main():
     filename_list = []
     output_list = []
     show_board_list = []
-    for i in range(proc * iters):
-        output_list.append(True if i % proc == 0 else False)
+    for i in range(1, 22):
+        output_list.append(True) # if i % proc == 0 else False)
         show_board_list.append(True if i==0 else False)
-        params['seed_ga'] = random.randint(1, 10_000)
+        params['pm'] = i*0.05 # random.randint(1, 100_000)
+        if i == 21: params['pm'] = 0.01
         params_list.append(params.copy())
-        filename_list.append("results/{}".format(i + bias))
+        filename_list.append("results/pm{}".format(i + bias))
     with mp.Pool(proc) as pool:
         pool.starmap(run_ga, zip(params_list, filename_list, output_list, show_board_list))
 
